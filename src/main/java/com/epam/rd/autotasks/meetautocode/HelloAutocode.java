23ae4fa91249b0e43fa7c74e597b683ad88a6203
anastasiia_vecherkovska@epam.com <anastasiia_vecherkovska@epam.com>
2022-06-06 18:49:56 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
@@ -4,5 +4,6 @@ public class HelloAutocode {
     public static void main(String[] args) {
         //Write a program, printing "Hello, Autocode!"
         System.out.println("Hello, Autocode!");
+        System.out.println("Hello, Autocode!");
     }
 }
